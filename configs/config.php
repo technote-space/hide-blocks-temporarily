d0e1fe4549eed54531f5f9f6b581fe7f03829ac7
Technote <technote.space@gmail.com>
2019-04-28 21:00:32 +0900

    remove update check

--- configs/config.php
+++ configs/config.php
@@ -17,9 +17,6 @@ return [
 	// required wordpress version
 	'required_wordpress_version' => '5.1', // for gutenberg
 
-	// update
-	'update_info_file_url'       => 'https://raw.githubusercontent.com/technote-space/hide-blocks-temporarily/master/update.json',
-
 	// github repo
 	'github_repo'                => 'technote-space/hide-blocks-temporarily',
 ];
