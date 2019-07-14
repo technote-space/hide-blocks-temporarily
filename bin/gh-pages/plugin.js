d1de5d910da1ec2e9d4af66f7a7efa76b89e8898
Technote <technote.space@gmail.com>
2019-07-15 04:42:09 +0900

    chore: trivial changes

--- bin/gh-pages/plugin.js
+++ bin/gh-pages/plugin.js
@@ -6,6 +6,7 @@ import './plugin.scss';
 window.hbtParams = window.hbtParams || {};
 
 addFilter( 'gh-pages.renderContent', 'plugin/renderContent', () => <Fragment>
+	<p>This page is demonstration of <a href="https://github.com/technote-space/hide-blocks-temporarily">Hide Blocks Temporarily</a></p>
 	<p>You can switch visibility of each block.</p>
 	<img className='playground__content__screenshot' src='./screenshot.gif'/>
 </Fragment> );
