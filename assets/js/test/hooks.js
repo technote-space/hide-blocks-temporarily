0119baf8a709df0b35ccc2ea6bd25f22fa0a233b
Technote <technote.space@gmail.com>
2019-08-14 15:12:46 +0900

    chore: trivial changes

--- assets/js/test/hooks.js
+++ assets/js/test/hooks.js
@@ -2,7 +2,7 @@ require( 'should' );
 import { getNamespace } from '../src/utils';
 import { PLUGIN_NAME } from '../src/constant';
 
-describe( 'getNamespace test', () => {
+describe( 'getNamespace', () => {
 	it( 'should get namespace', () => {
 		const nameSpace = getNamespace( 'test' );
 		nameSpace.should.startWith( PLUGIN_NAME );
