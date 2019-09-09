8dab7cd780876fead89643a9a062dd0a489f6025
Technote <technote.space@gmail.com>
2019-09-09 13:19:40 +0900

    faet: update test

--- assets/js/test/setup.js
+++ assets/js/test/setup.js
@@ -8,11 +8,47 @@ global.window.matchMedia = () => ( {
 	matches: true, addListener: () => {
 	},
 } );
+global.wpMock = {
+	blockEditor: {
+		getColorObjectByColorValue: () => false,
+	},
+	element: {
+		useRef: () => ( {
+			current: {
+				contains: () => false,
+				focus: () => 0,
+				getBoundingClientRect: () => ( { width: 0, height: 0 } ),
+				parentNode: {
+					getBoundingClientRect: () => ( { width: 0, height: 0, left: 0, right: 0, top: 0, bottom: 0 } ),
+				},
+				querySelectorAll: () => ( [] ),
+			},
+		} ),
+	},
+};
+global.window.lodash.debounce = fn => {
+	function debounced() {
+		return fn();
+	}
+
+	debounced.cancel = jest.fn();
+	debounced.flush = jest.fn();
+	return debounced;
+};
 global.hbtParams = {
 	translate: {
 		test: 'テスト',
 	},
 };
+
+jest.mock( '@wordpress/block-editor', () => ( {
+	...jest.requireActual( '@wordpress/block-editor' ),
+	getColorObjectByColorValue: ( colors, value ) => global.wpMock.blockEditor.getColorObjectByColorValue( colors, value ),
+} ) );
+jest.mock( '@wordpress/element', () => ( {
+	...jest.requireActual( '@wordpress/element' ),
+	useRef: ( colors, value ) => global.wpMock.element.useRef( colors, value ),
+} ) );
 
 const blockEditor = require( '@wordpress/block-editor' );
 const blockLibrary = require( '@wordpress/block-library' );
@@ -20,10 +56,12 @@ const blocks = require( '@wordpress/blocks' );
 const components = require( '@wordpress/components' );
 const coreData = require( '@wordpress/core-data' );
 const data = require( '@wordpress/data' );
+const dom = require( '@wordpress/dom' );
 const editPost = require( '@wordpress/edit-post' );
 const editor = require( '@wordpress/editor' );
 const element = require( '@wordpress/element' );
 const i18n = require( '@wordpress/i18n' );
+const isShallowEqual = require( '@wordpress/is-shallow-equal' );
 const richText = require( '@wordpress/rich-text' );
 const url = require( '@wordpress/url' );
 
@@ -34,10 +72,12 @@ global.wp = {
 	components,
 	coreData,
 	data,
+	dom,
 	editPost,
 	editor,
 	element,
 	i18n,
+	isShallowEqual,
 	richText,
 	url,
 };
