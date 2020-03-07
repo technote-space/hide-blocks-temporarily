789da0785f4cb57ee0951673320213faf4fe420a
Technote <technote.space@gmail.com>
2020-03-08 02:06:28 +0900

    chore: update wp version

--- autoload.php
+++ autoload.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/technote-space/hide-blocks-temporarily/
  * Description: This plugin makes it easy to hides blocks temporarily.
  * Author: Technote
- * Version: 1.3.1
+ * Version: next-v1.3.1
  * Author URI: https://technote.space
  * Text Domain: hide-blocks-temporarily
  * Domain Path: /languages/
